f3ec2e32a222bd8c964415c949bc3bed6c45ce68
MeGaLiGhT14 <sasha.bodrov.99@yandex.ru>
2020-05-12 16:58:09 +0300

    Loading libraries 2

---
+++ Assets/Zone UI/Scripts/CursorSwitch.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Michsky.UI.Zone
+{
+    public class CursorSwitch : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> 
+    }
+}
